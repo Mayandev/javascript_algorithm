5f2cf0366c05786ae2cf7b194c5766d93db54e82
mayandev <phillzou@gmail.com>
2019-11-11 19:48:33 +0800

    code: 新增find方法

--- 算法与数据结构学习/data-structures/linked-list/LinkedList.js
+++ 算法与数据结构学习/data-structures/linked-list/LinkedList.js
@@ -153,6 +153,36 @@ export default class LinkedList {
   }
 
   /**
+   * @param {Object} findParams
+   * @param {*} findParams.value
+   * @param {function} [findParams.callback]
+   * @return {LinkedListNode}
+   */
+  find({ value = undefined, callback = undefined }) {
+    if (!this.head) {
+      return null;
+    }
+
+    let currentNode = this.head;
+
+    while (currentNode) {
+      // If callback is specified then try to find node by callback.
+      if (callback && callback(currentNode.value)) {
+        return currentNode;
+      }
+
+      // If value is specified then try to compare by value..
+      if (value !== undefined && currentNode.value === value) {
+        return currentNode;
+      }
+
+      currentNode = currentNode.next;
+    }
+
+    return null;
+  }
+
+  /**
    * 旋转链表
    * @returns {LinkedList}
    */
